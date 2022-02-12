9e37044c259c45b6aa921ecdeccde7b4100babef
msimo9 <matjaz.simonic@os-ajdovscina.si>
2022-02-12 17:16:29 +0100

    Added timer slider and logic

--- components/TypingTest.js
+++ components/TypingTest.js
@@ -7,7 +7,9 @@ import Information from './Information';
 import ToggleModalButton from './ToggleModalButton';
 import Finished from './Finished';
 
-import {useSelector} from 'react-redux'
+import {useSelector, useDispatch} from 'react-redux'
+
+import { changeWpm, incrementErrors } from '../redux/redux';
 
 const TypingTest = () => {
     let initalArr = [''];
@@ -20,19 +22,24 @@ const TypingTest = () => {
     const [hasStarted, setHasStarted] = useState(false);
 
     const [currentChar, setCurrentChar] = useState('');
-    const [errors, setErrors] = useState(0);
-    const [wpm, setWpm] = useState(0);
+    const [errors, setErrors] = useState(useSelector(state => state.errors));
+    const [wpm, setWpm] = useState(useSelector(state => state.wpm));
     const [typedWords, setTypedWords] = useState(0);
-
     const [hasFinished, setHasFinished] = useState(false);
+    const timer = useSelector(state => state.timer);
+
+    const initialTimerValue = useSelector(state => state.timerValue);
+    const [timerValue, setCountdownTimer] = useState(initialTimerValue);
 
     const numberOfWords = useSelector(state => state.sliderValue);
     const language = useSelector(state => state.language);
     const fetchString = "https://random-word-api.herokuapp.com/word?number="+numberOfWords+"&lang="+language;
     console.log(numberOfWords);
 
+    const dispatch = useDispatch()
+
     const getData = () => {
-        fetch(fetchString)
+        fetch(fetchString, {mode: 'cors'})
         .then(response => response.json())
         .then(data => setRandomWords(data))
         .then(setIsReady(true));
@@ -72,15 +79,35 @@ const TypingTest = () => {
             interval2 = setInterval(() => {
                 setTotalCounter(totalCounter => totalCounter+1);
                 console.log(totalCounter);
-                if(totalCounter > 3){
-                    setWpm(Math.floor(typedWords/(totalCounter/60)));
-                    console.log(wpm);
+                if(totalCounter > 3 && !hasFinished){
+                    let tempWpm = Math.floor(typedWords/(totalCounter/60));
+                    setWpm(tempWpm);
+                    dispatch(changeWpm(tempWpm));
                 }
             }, 1000)
         }
 
         return (() => clearInterval(interval2));
     }, [totalCounter, hasStarted])
+
+    useEffect(() => {
+        let interval3;
+
+        if(hasStarted && timer){
+            interval3 = setInterval(() => {
+                setCountdownTimer(timerValue => timerValue-1)
+                if(timerValue == 0){
+                    setHasFinished(true);
+                }
+            }, 1000)
+        }
+
+        return (() => clearInterval(interval3));
+    }, [timerValue, hasStarted])
+
+    useEffect(() => {
+        setCountdownTimer(initialTimerValue)
+    }, [timer, initialTimerValue]);
 
     useEffect(() => {
 
@@ -93,6 +120,7 @@ const TypingTest = () => {
         if(key === " "){
             getData();
             setHasFinished(false);
+            setCountdownTimer(initialTimerValue);
         }
 
         if(key == "Backspace"){
@@ -101,7 +129,7 @@ const TypingTest = () => {
             setText(text+key);
         }
         console.log(key);
-        if(currentChar == key && key!=" " && randomWords.length != 0){
+        if(currentChar == key && key!=" " && randomWords.length != 0 && !hasFinished){
             setHasStarted(true);
             randomWords[0] = randomWords[0].substring(1);
             if(randomWords[0].length == 0){
@@ -117,6 +145,7 @@ const TypingTest = () => {
             }
         }else if(key != " "){
             setErrors(errors => errors + 1);
+            dispatch(incrementErrors());
         }
     });
     if(!hasFinished){
@@ -126,6 +155,7 @@ const TypingTest = () => {
             style={styles.container}
         >   
             <Information errors={errors} wpm={wpm}/>
+            {timer ? <Text style={styles.timerValue}>time left: {timerValue}</Text> : null}
             <View style={styles.wordsContainer}>
                 {randomWords.slice(0, 15).map((item, index) => {
                     if(index == 0){
@@ -191,6 +221,13 @@ const styles = StyleSheet.create({
         fontWeight: 500,
         fontFamily: 'RobotoMono_100Thin',
         marginHorizontal: 5,
+    },
+    timerValue:{
+        color: "white",
+        fontWeight: 500,
+        fontFamily: 'RobotoMono_100Thin',
+        marginBottom: 25,
+        fontSize: 16,
     },
     textinput:{
         width: 500
